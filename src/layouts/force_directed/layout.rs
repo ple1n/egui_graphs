73da1a2c51781a16c4965c2c5d5fc5de6a4255ff
plein <68096727+planetoryd@users.noreply.github.com>
2025-07-26 16:25:36 +0800

    adjsut

--- src/layouts/force_directed/layout.rs
+++ src/layouts/force_directed/layout.rs
@@ -87,7 +87,7 @@ where
             Ix,
         > = g.g_mut();
         #[allow(clippy::unreadable_literal)]
-        let coeff: usize = clamp(((self.v / 0.00000000000002).round() as usize), 1, 30);
+        let coeff: usize = clamp(((self.v / 0.0000000000002).round() as usize), 0, 30);
 
         self.algo.apply_many(gx, coeff);
         let mut vavg: SVector<_, 2> = Default::default();
